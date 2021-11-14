65f74003c459dd5246ef41589f2e714f30ac56e9
silsu876 <87000648+silsu876@users.noreply.github.com>
2021-11-15 02:27:04 +0900

    캐릭터 벽 막기, 해당 좌표로 가면 이벤트 뜨기

--- move/src/application/Main.java
+++ move/src/application/Main.java
@@ -1,12 +1,17 @@
 package application;
-	
+
+import java.io.File;
+
+import cont.ImageAnimationView;
+import javafx.animation.Animation;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
@@ -14,16 +19,37 @@ public class Main extends Application {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("/cont/Image.fxml"));
-			AnchorPane root = (AnchorPane)loader.load();
-			Scene scene = new Scene(root,1200,800);
+			AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root, 1200, 800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+
+//			// 이미지 애니메이션용 클래스 사용
+//			ImageAnimationView img = new ImageAnimationView(Duration.millis(1000), new File("nono.png"), 30, 58);
+//
+//			// 걷는 그래픽 범위 표시
+//			img.setAnimationRange(1, img.getMaxIndex());
+//
+//			// 무한 루프 애니메이션 시작
+//			img.setCycleCount(Animation.INDEFINITE);
+//			img.play();
+//
+//			// 이미지가 작으면 확대하자.
+//			img.setScaleX(3);
+//			img.setScaleY(3);
+//
+//			// 중앙에 배치하여 표시
+//			Bounds b = img.getBoundsInLocal();
+//			img.setX(scene.getWidth() / 2 - b.getWidth() / 2);
+//			img.setY(scene.getHeight() / 2 - b.getHeight() / 2);
+//			root.getChildren().add(img);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- move/src/cont/Cont.java
+++ move/src/cont/Cont.java
@@ -9,53 +9,145 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
 
 public class Cont implements Initializable {
 	@FXML
-	private ImageView mario;
+	private ImageView imageView;
+	@FXML
+	private Rectangle rectangle;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		/* 키보드 입력에 따라 이미지 위치 변경 */
-		mario.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent event) {
-				handleImage(event);
+				pressed(event, setStopPoint());
+			}
+		});
+		imageView.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+//				Released(event);
 			}
 		});
 
 	}
 
-	/* 이미지 위치변경 메소드 */
-	public void handleImage(KeyEvent event) {
+	public void pressed(KeyEvent event, String moveStop) {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
 		KeyCode keyCode = event.getCode();
-		if (keyCode.equals(KeyCode.RIGHT)) {
-			mario.setX(mario.getX() + 10);
-		} else if (keyCode.equals(KeyCode.LEFT)) {
-			mario.setX(mario.getX() - 10);
-		} else if (keyCode.equals(KeyCode.UP)) {
-			mario.setY(mario.getY() - 10);
-		} else if (keyCode.equals(KeyCode.DOWN)) {
-			mario.setY(mario.getY() + 10); 
+		if (moveStop.equals("up")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("down")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			}
+		} else if (moveStop.equals("left")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("right")) {
+			if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("leftup")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("rightup")) {
+			if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("leftdown")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			}
+		} else if (moveStop.equals("rightdown")) {
+			if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			}
+		} else {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		}
+
+		// 해당 좌표 갔을때 이벤트
+		if (((x <= (int) rectangle.getX() + 10) && (x >= (int) rectangle.getX() - 10))
+				&& ((y <= (int) rectangle.getY() + 10) && (y >= (int) rectangle.getY() - 10))) {
+			System.out.println("이벤트 발생!");
 		}
 	}
+
+	public String setStopPoint() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+
+		if (y <= 15) {
+			if (x <= 0) {
+				return "leftup";
+			}
+			if (x >= 1100) {
+				return "rightup";
+			}
+			return "up";
+		} else if (y >= 635) {
+			if (x <= 0) {
+				return "leftdown";
+			}
+			if (x >= 1100) {
+				return "rightdown";
+			}
+			return "down";
+		} else if (x <= 0) {
+			return "left";
+		} else if (x >= 1100) {
+			return "right";
+		}
+		return "";
+
+	}
+//혹시몰라서 남겨둔 키 땠을때 이벤트
+//	public void Released(KeyEvent event) {
+//		KeyCode keyCode = event.getCode();
+//		if (keyCode.equals(KeyCode.RIGHT)) {
+//		} else if (keyCode.equals(KeyCode.LEFT)) {
+//		} else if (keyCode.equals(KeyCode.UP)) {
+//		} else if (keyCode.equals(KeyCode.DOWN)) {
+//		}
+//	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
