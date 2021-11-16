e1e6605b83df1bb796f0a420cbb6897b7cba16f4
silsu876 <87000648+silsu876@users.noreply.github.com>
2021-11-16 18:30:29 +0900

    아직 지하랑 3층 쪽방 두개 안만듬

--- TeamProjectWork/src/gameStart/B1Move.java
+++ TeamProjectWork/src/gameStart/B1Move.java
@@ -1,5 +1,190 @@
 package gameStart;
 
-public class B1Move {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class B1Move implements Initializable{
+	@FXML
+	private ImageView imageView;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				pressed(event, setStopPoint());
+			}
+		});
+		imageView.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+//				Released(event);
+			}
+		});
+
+	}
+
+	public void pressed(KeyEvent event, String moveStop) {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+		KeyCode keyCode = event.getCode();
+		if (moveStop.equals("up")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("down")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			}
+		} else if (moveStop.equals("left")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("right")) {
+			if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("leftup")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("rightup")) {
+			if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		} else if (moveStop.equals("leftdown")) {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			}
+		} else if (moveStop.equals("rightdown")) {
+			if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			}
+		} else {
+			if (keyCode.equals(KeyCode.RIGHT)) {
+				imageView.setX(x + 10);
+			} else if (keyCode.equals(KeyCode.LEFT)) {
+				imageView.setX(x - 10);
+			} else if (keyCode.equals(KeyCode.UP)) {
+				imageView.setY(y - 10);
+			} else if (keyCode.equals(KeyCode.DOWN)) {
+				imageView.setY(y + 10);
+			}
+		}
+
+//이벤트 발생/ 나중에 그림에 퍼즐 추가시 재 설정
+//		if ((y == 100) && (x >= 270) && (x <= 320)) {
+//
+//			try {
+//				Parent root;
+//				root = FXMLLoader.load(getClass().getResource("/puzzle/puzzleLayout.fxml"));
+//				Scene scene = new Scene(root);
+//				Stage primaryStage = (Stage) imageView.getScene().getWindow();
+//				primaryStage.setScene(scene);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//
+//		}
+
+	}
+
+	public String setStopPoint() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+
+
+		// 싱크대
+		if ((y == 220) && (x <= 155) && (x >= 5)) {
+			if (x == 5) {
+				return "leftdown";
+			}
+			return "down";
+		} else if ((x == 155) && (y <= 620) && (y >= 230)) {
+			if (y == 620) {
+				return "leftdown";
+			}
+			return "left";
+		}
+//		테이블
+		if ((y == 300) && (x >= 685) && (x <= 735)) {
+			if (x == 735) {
+				return "rightdown";
+			}
+			return "down";
+		} else if ((x == 735) && (y <= 300) && (y >= 230)) {
+			return "right";
+		} else if ((y == 230) && (x >= 745) && (x <= 935)) {
+			return "down";
+		} else if ((x == 945) && (y >= 240) && (y <= 290)) {
+			return "left";
+		} else if ((y == 300) && (x >= 945) && (x <= 1015)) {
+			if (x == 945) {
+				return "leftdown";
+			}
+			return "down";
+		} else if ((x == 1025) && (y >= 310) && (y <= 570)) {
+			return "left";
+		} else if ((y == 580) && (x <= 1015) && (x >= 675)) {
+			return "up";
+		} else if ((x == 665) && (y >= 310) && (y <= 570)) {
+			return "right";
+		}
+
+//		전체 배경 안빠져나가게
+		if (y <= 110) {
+			if (x <= 10) {
+				return "leftup";
+			}
+			if (x >= 1090) {
+				return "rightup";
+			}
+			return "up";
+		} else if (y >= 620) {
+			if (x <= 10) {
+				return "leftdown";
+			}
+			if (x >= 1090) {
+				return "rightdown";
+			}
+			return "down";
+		} else if (x <= 10) {
+			return "left";
+		} else if (x >= 1090) {
+			return "right";
+		}
+		return "";
+
+	}
 }
