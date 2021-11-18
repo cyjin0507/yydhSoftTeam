d5b914cf39c60e80f7b0547c08dc872189dfc407
silsu876 <87000648+silsu876@users.noreply.github.com>
2021-11-18 17:10:35 +0900

    수정

--- TeamProjectWork/src/gameStart/MainHoll.java
+++ TeamProjectWork/src/gameStart/MainHoll.java
@@ -6,9 +6,11 @@ import java.util.ResourceBundle;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
 import mainGame.chat;
 
 public class MainHoll extends chat implements Initializable {
@@ -16,6 +18,7 @@ public class MainHoll extends chat implements Initializable {
 	private ImageView user1; 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		sprite();
 		user1.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent event) {
@@ -23,8 +26,14 @@ public class MainHoll extends chat implements Initializable {
 				System.out.println("testdfg");
 			}
 		});
+		user1.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				animation.stop();
+			}
+		});
 	}
-	
+	SpriteAnimation animation;
 	
 	public void pressed(KeyEvent event) {
 		System.out.println("ters");
@@ -32,15 +41,26 @@ public class MainHoll extends chat implements Initializable {
 		int y = (int) user1.getY();
 		KeyCode keyCode = event.getCode();
 		if (keyCode.equals(KeyCode.RIGHT)) {
+			animation.play();
+			animation.setOffsetY(300);
 			user1.setX(x + 10);
 		} else if (keyCode.equals(KeyCode.LEFT)) {
+			animation.play();
+			animation.setOffsetY(150);
 			user1.setX(x - 10);
 		} else if (keyCode.equals(KeyCode.UP)) {
+			animation.play();
+			animation.setOffsetY(450);
 			user1.setY(y - 10);
 		} else if (keyCode.equals(KeyCode.DOWN)) {
+			animation.play();
+			animation.setOffsetY(0);
 			user1.setY(y + 10);
 		}
 	}
 	
-
+	public void sprite() {
+		user1.setImage(new Image("/roomImage/all.png"));
+		animation = new SpriteAnimation(user1, Duration.millis(500), 3, 4, 0, 0, 100, 150);
+	}
 }
