8d5ddbc63e9d365b163286c1e07a0fbe11107d34
silsu876 <87000648+silsu876@users.noreply.github.com>
2021-11-19 02:46:28 +0900

    캐릭터 모션이랑 클래스 합친거

--- TeamProjectWork/src/gameStart/CharacterMove.java
+++ TeamProjectWork/src/gameStart/CharacterMove.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.mysql.cj.x.protobuf.MysqlxExpect.Open.Condition.Key;
+
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +16,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -24,13 +27,67 @@ public class CharacterMove implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		String image = imageView.getId();
 		sprite();
-		imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				pressed(event, setStopPoint());
-			}
-		});
+		if (image.equals("floor1")) {
+			imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					pressed(event, floor1setStopPoint());
+					floor1event();
+				}
+			});
+			stop();
+		} else if (image.equals("floor2")) {
+			imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					pressed(event, floor2setStopPoint());
+					floor2event();
+				}
+			});
+			stop();
+		} else if (image.equals("floor3")) {
+			imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					pressed(event, floor3setStopPoint());
+					floor3event();
+				}
+			});
+			stop();
+		} else if (image.equals("floor3_1")) {
+			imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					pressed(event, floor3_small1setStopPoint());
+					floor3_small1event();
+				}
+			});
+			stop();
+		} else if (image.equals("floor3_2")) {
+			imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					pressed(event, floor3_small2setStopPoint());
+					floor3_small2event();
+				}
+			});
+			stop();
+		} else if (image.equals("B1")) {
+			imageView.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent event) {
+					pressed(event, B1setStopPoint());
+					B1event();
+				}
+			});
+			stop();
+		}
+
+	}
+// 모션 스땁
+	public void stop() {
 		imageView.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent event) {
@@ -38,8 +95,6 @@ public class CharacterMove implements Initializable {
 			}
 		});
 	}
-
-	SpriteAnimation animation;
 
 //눌렀을때 움직이는거
 	public void pressed(KeyEvent event, String moveStop) {
@@ -116,6 +171,8 @@ public class CharacterMove implements Initializable {
 		}
 	}
 
+	public SpriteAnimation animation;
+
 //위 아래 왼쪽 오른쪽
 	public void up(int y) {
 
@@ -164,9 +221,10 @@ public class CharacterMove implements Initializable {
 
 //캐릭터 모션
 	public void sprite() {
-		imageView.setImage(new Image("/image/all.png"));
+		imageView.setImage(new Image("/roomImage/all.png"));
 		animation = new SpriteAnimation(imageView, Duration.millis(500), 3, 4, 0, 0, 100, 150);
 	}
+
 //기본 틀
 	public String frame(int x, int y) {
 		if (y <= 110) {
@@ -192,8 +250,234 @@ public class CharacterMove implements Initializable {
 		}
 		return "";
 	}
+
+	// 쪽방 틀
+	public String smallframe(int x, int y) {
+//		전체 배경 안빠져나가게
+		if (y <= 120) {
+			if (x <= 10) {
+				return "leftup";
+			}
+			if (x >= 600) {
+				return "rightup";
+			}
+			return "up";
+		} else if (y >= 620) {
+			if (x <= 10) {
+				return "leftdown";
+			}
+			if (x >= 600) {
+				return "rightdown";
+			}
+			return "down";
+		} else if (x <= 10) {
+			return "left";
+		} else if (x >= 600) {
+			return "right";
+		}
+		return "";
+
+	}
+
 //====================================================
-	
+	// 3층 쪽방 2 멈추는거
+	public String floor3_small2setStopPoint() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+		// 책장
+		if ((y == 180) && (x >= 10) && (x <= 130)) {
+			if (x == 10) {
+				return "leftup";
+			}
+			return "up";
+		} else if ((x == 140) && (y <= 170) && (y >= 130)) {
+			if (y == 130) {
+				return "leftup";
+			}
+			return "left";
+		}
+		// 빗자루
+		if ((x == 530) && (y >= 130) && (y <= 150)) {
+			if (y == 130) {
+				return "rightup";
+			}
+			return "right";
+		} else if ((y == 160) && (x >= 540) && (x <= 600)) {
+			if (x == 600) {
+				return "rightup";
+			}
+			return "up";
+		}
+
+		smallframe(x, y);
+		return "";
+
+	}
+
+	// 3층 쪽방 2이벤트
+	public void floor3_small2event() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+		if ((y == 130) && (x >= 260) && (x <= 370)) {
+			try {
+				Parent root;
+				root = FXMLLoader.load(getClass().getResource("/btngame/MainLayout.fxml"));
+				Scene scene = new Scene(root);
+				Stage primaryStage = (Stage) imageView.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	// 3층 쪽방 1 멈추는거
+	public String floor3_small1setStopPoint() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+
+		// 세면대
+		if ((x == 220) && (y >= 100) && (y <= 120)) {
+			if (y >= 100) {
+				return "rightup";
+			}
+			return "right";
+		} else if ((y == 130) && (x >= 230) && (x <= 330)) {
+			return "up";
+		} else if ((x == 340) && (y >= 100) && (y <= 120)) {
+			if (y == 100) {
+				return "leftup";
+			}
+			return "left";
+		}
+		// 변기
+		if ((x == 600) && (y >= 100) && (y <= 160)) {
+			if (y == 100) {
+				return "leftup";
+			}
+			return "left";
+		} else if ((x <= 590) && (y == 170) && (x >= 440)) {
+			return "up";
+		} else if ((x == 430) && (y >= 100) && (y <= 160)) {
+			if (y == 100) {
+				return "rightup";
+			}
+			return "right";
+		}
+		smallframe(x, y);
+		return "";
+
+	}
+
+	// 3층 쪽방 1이벤트
+	public void floor3_small1event() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+		if ((y == 100) && (x <= 100) && (x >= 40)) {
+			try {
+				Parent root;
+				root = FXMLLoader.load(getClass().getResource("/pipegame/layout.fxml"));
+				Scene scene = new Scene(root);
+				Stage primaryStage = (Stage) imageView.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	// 3층 멈추는거
+	public String floor3setStopPoint() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+//		책상 충돌 방지
+		if ((y == 270) && (x >= 360) && (x <= 750)) {
+			if ((x == 660) && (y <= 270) && (y >= 170)) {
+				return "leftdown";
+			}
+			if ((x == 470) && (y <= 270) && (y >= 170)) {
+				return "rightdown";
+			}
+			return "down";
+		} else if ((y >= 270) && (y <= 520) && (x == 370)) {
+			return "right";
+		} else if ((y == 520) && (x >= 360) && (x <= 750)) {
+			return "up";
+		} else if ((y >= 270) && (y <= 520) && (x == 760)) {
+			return "left";
+		}
+//		의자 충돌
+		if ((x == 660) && (y <= 270) && (y >= 170)) {
+			return "leftdown";
+		} else if ((x == 470) && (y <= 270) && (y >= 170)) {
+			return "rightdown";
+		} else if ((y == 160) && (x <= 650) && (x >= 470)) {
+			return "down";
+		}
+//식물
+		if ((x == 120) && (y >= 460)) {
+			if (y == 620) {
+				return "leftdown";
+			}
+			return "left";
+		} else if ((y == 460) && (x <= 120)) {
+			if (x == 10) {
+				return "leftdown";
+			}
+			return "down";
+		}
+//옷걸이
+		if ((x == 970) && (y >= 440)) {
+			if (y == 620) {
+				return "rightdown";
+			}
+			return "right";
+		} else if ((y == 440) && (x >= 970)) {
+			if (x == 1090) {
+				return "rightdown";
+			}
+			return "down";
+		}
+//책꽂이
+		if ((x == 370) && (y <= 130) && (y >= 100)) {
+			if (y == 100) {
+				return "rightup";
+			}
+			return "right";
+		} else if ((x == 740) && (y <= 130) && (y >= 100)) {
+			if (y == 100) {
+				return "leftup";
+			}
+			return "left";
+		} else if ((x >= 380) && (x <= 720) && (y == 140)) {
+			return "up";
+		}
+		frame(x, y);
+		return "";
+
+	}
+
+	// 3층 이벤트
+	public void floor3event() {
+		int x = (int) imageView.getX();
+		int y = (int) imageView.getY();
+		if ((y == 100) && (x >= 270) && (x <= 320)) {
+
+			try {
+				Parent root;
+				root = FXMLLoader.load(getClass().getResource("/puzzle/puzzleLayout.fxml"));
+				Scene scene = new Scene(root);
+				Stage primaryStage = (Stage) imageView.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
 	// 2층 멈추는거
 	public String floor2setStopPoint() {
 		int x = (int) imageView.getX();
@@ -213,14 +497,14 @@ public class CharacterMove implements Initializable {
 			return "right";
 		} else if ((y == 305) && (x >= 360) && (x <= 730)) {
 			return "up";
-		} else if ((x ==740) && (y <= 295) && (y >= 215)) {
+		} else if ((x == 740) && (y <= 295) && (y >= 215)) {
 			return "left";
-		} else if((y == 205)&& (x >= 740) && (x <= 870)) {
+		} else if ((y == 205) && (x >= 740) && (x <= 870)) {
 			if (x == 740) {
 				return "leftup";
 			}
 			return "up";
-		} else if((x ==880) && (y <= 195) && (y >= 115)) {
+		} else if ((x == 880) && (y <= 195) && (y >= 115)) {
 			if (y == 115) {
 				return "leftup";
 			}
--- move/src/cont/Cont.java
+++ move/src/cont/Cont.java
@@ -1,7 +1,6 @@
 package cont;
 
 import java.net.URL;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
