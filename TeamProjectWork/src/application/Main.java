2de94619a900d215bddd916fd1640772f5257204
choiyoungjin <cyjin0507@gmail.com>
2021-11-25 16:08:55 +0900

    화면 크기 조절 불가능하게 바꿈

--- TeamProjectWork/src/application/Main.java
+++ TeamProjectWork/src/application/Main.java
@@ -39,6 +39,8 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			primaryStage.setResizable(false);
 
 			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				@Override
