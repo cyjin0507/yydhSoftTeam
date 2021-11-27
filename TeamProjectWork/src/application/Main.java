9a7758ea2f8cdfe2f454969b3f04d2380068bcd8
cyjin0507 <cyjin0507@gmail.com>
2021-11-27 15:28:47 +0900

    뭔가 바뀜

--- TeamProjectWork/src/application/Main.java
+++ TeamProjectWork/src/application/Main.java
@@ -26,9 +26,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-//			loader.setLocation(getClass().getResource("/user/Login.fxml"));
+			loader.setLocation(getClass().getResource("/user/Login.fxml"));
 
-			loader.setLocation(getClass().getResource("/B1room/storage.fxml"));
+//			loader.setLocation(getClass().getResource("/B1room/storage.fxml"));
 //			loader.setLocation(getClass().getResource("/gameStart/mainhall.fxml"));
 //			loader.setLocation(getClass().getResource("/floor1room/library.fxml"));
 //			loader.setLocation(getClass().getResource("/floor2room/study.fxml"));
