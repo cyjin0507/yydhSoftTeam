ca46e13c3592bd0c67b546a87662967f28ed44b2
cyjin0507 <cyjin0507@gmail.com>
2021-11-27 17:12:26 +0900

    뭔가 바뀜

--- TeamProjectWork/src/user/mainPage.java
+++ TeamProjectWork/src/user/mainPage.java
@@ -2,19 +2,35 @@ package user;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import util.JDBCUtil;
 
-public class mainPage {
+public class mainPage implements Initializable {
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		MediaPlayer mp;
+		Media m = null;
+		
+		mp = new MediaPlayer(new Media(getClass().getResource("/music/메인화면.mp3").toString()));
+        mp.play();
+    
+	}
 
 	@FXML
 	private Button gameStart;
@@ -83,5 +99,7 @@ public class mainPage {
 			e.printStackTrace();
 		}
 	}
+
+	
 
 }
