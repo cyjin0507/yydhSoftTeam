c17a7ea0ae4b68344d6698d45a94d95c464101b7
silsu876 <87000648+silsu876@users.noreply.github.com>
2021-11-28 21:55:13 +0900

    수정

--- TeamProjectWork/src/floor2room/passward.java
+++ TeamProjectWork/src/floor2room/passward.java
@@ -16,65 +16,75 @@ import javafx.stage.Stage;
 public class passward {
 
 	@FXML
-	private Label first; //1000
+	private Label first; // 1000
 	@FXML
-	private Label second; //0100
+	private Label second; // 0100
 	@FXML
 	private Label third; // 0010
 	@FXML
-	private Label fourth; //0001
-	
+	private Label fourth; // 0001
+
 	private static int current = 1;
 	public static int ans = 0;
-	
+
 	public void zero() {
 		countUp(0);
 	}
+
 	public void one() {
 		countUp(1);
 	}
+
 	public void two() {
 		countUp(2);
 	}
+
 	public void three() {
 		countUp(3);
 	}
+
 	public void four() {
 		countUp(4);
 	}
+
 	public void five() {
 		countUp(5);
 	}
+
 	public void six() {
 		countUp(6);
 	}
+
 	public void seven() {
 		countUp(7);
 	}
+
 	public void eight() {
 		countUp(8);
 	}
+
 	public void nine() {
 		countUp(9);
 	}
+
 	public void ten() {
 		countUp(10);
 	}
 
 	public void countUp(int num) {
-		if(current == 1) {
+		if (current == 1) {
 			fourth.setText(Integer.toString(num));
 			current++;
-		} else if(current == 2) {
+		} else if (current == 2) {
 			third.setText(fourth.getText());
 			fourth.setText(Integer.toString(num));
 			current++;
-		} else if(current == 3) {
+		} else if (current == 3) {
 			second.setText(third.getText());
 			third.setText(fourth.getText());
 			fourth.setText(Integer.toString(num));
 			current++;
-		} else if(current == 4) {
+		} else if (current == 4) {
 			first.setText(second.getText());
 			second.setText(third.getText());
 			third.setText(fourth.getText());
@@ -82,21 +92,21 @@ public class passward {
 			current++;
 		}
 	}
-	
-	public void countDown() {	
-		if(current == 2) {
+
+	public void countDown() {
+		if (current == 2) {
 			fourth.setText("0");
 			current--;
-		} else if(current == 3) {
+		} else if (current == 3) {
 			fourth.setText(third.getText());
 			third.setText("0");
 			current--;
-		} else if(current == 4) {
+		} else if (current == 4) {
 			fourth.setText(third.getText());
 			third.setText(second.getText());
 			second.setText("0");
 			current--;
-		} else if(current == 5) {
+		} else if (current == 5) {
 			fourth.setText(third.getText());
 			third.setText(second.getText());
 			second.setText(first.getText());
@@ -105,6 +115,7 @@ public class passward {
 		}
 
 	}
+
 	public void back() {
 		if(ans == 3479) {
 			try {
@@ -116,7 +127,7 @@ public class passward {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-<<<<<<< Updated upstream
+
 		}else if(ans == 4812) {
 			try {
 				Parent root;
@@ -139,16 +150,13 @@ public class passward {
 			}
 		}
 	}
-	
-=======
-		}
-	}
->>>>>>> Stashed changes
-	public void enter() {
-		int number = Integer.parseInt(first.getText()) * 1000 +Integer.parseInt(second.getText()) * 100 + Integer.parseInt(third.getText()) * 10 + Integer.parseInt(fourth.getText());
 
-		if(ans == number) {
-			if(ans == 3479) {
+	public void enter() {
+		int number = Integer.parseInt(first.getText()) * 1000 + Integer.parseInt(second.getText()) * 100
+				+ Integer.parseInt(third.getText()) * 10 + Integer.parseInt(fourth.getText());
+
+		if (ans == number) {
+			if (ans == 3479) {
 				try {
 					Parent root;
 					root = FXMLLoader.load(getClass().getResource("/floor2room/drawingroom.fxml"));
@@ -158,7 +166,7 @@ public class passward {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			}else if(ans == 4812) { 
+			} else if (ans == 4812) {
 				try {
 					Parent root;
 					root = FXMLLoader.load(getClass().getResource("/floor3room/stair.fxml"));
@@ -168,7 +176,7 @@ public class passward {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-			}else if(ans ==1261) {
+			} else if (ans == 1261) {
 				try {
 					Parent root;
 					root = FXMLLoader.load(getClass().getResource("/floor2room/dressroom.fxml"));
@@ -181,6 +189,5 @@ public class passward {
 			}
 		}
 	}
-	
-	
+
 }
--- TeamProjectWork/src/gameStart/last1.java
+++ TeamProjectWork/src/gameStart/last1.java
@@ -18,9 +18,11 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import mainGame.gameReady;
 import mainGame.mediaview;
 import mainGame.server;
 import user.sort;
+import user.userController;
 import util.JDBCUtil;
 
 public class last1 extends sort implements Initializable {
@@ -38,6 +40,7 @@ public class last1 extends sort implements Initializable {
 					scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
 					primaryStage.setScene(scene);
 					gameDelete();
+					new gameReady().mp.stop();
 					new server().stopServer();
 				} else {
 					Alert alert = new Alert(AlertType.CONFIRMATION);
@@ -52,6 +55,7 @@ public class last1 extends sort implements Initializable {
 						Stage primaryStage = (Stage) ans.getScene().getWindow();
 						scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
 						primaryStage.setScene(scene);
+						new gameReady().mp.stop();
 						new server().stopServer();
 					} else if (result.get() == ButtonType.CANCEL) {
 						Parent root;
